a4216727e74a6bdf299fe2a96cba945ea5141fee
Kibrik <ariel@example.com>
2024-03-02 14:31:06 +0200

    Completly redesigned the forum page which required adjusting to it in both app.py and index.html

--- app.py
+++ app.py
@@ -13,42 +13,9 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     if 'username' in session:
-        return 'You are logged in as ' + session['username']
+        return render_template('index.html', category=session.get('category', 'Default Category'))
 
-    return render_template('index.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    users = mongo.db.users
-    login_user = users.find_one({'username' : request.form['username']})
-
-    if login_user:
-        if bcrypt.checkpw(request.form['password'].encode('utf-8'), login_user['password']):
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-
-    return 'Invalid username/password combination'
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'username' : request.form['username']})
-
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert_one({'username' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-
-        return 'That username already exists!'
-
-    return render_template('register.html')
-
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('index'))
+    return render_template('index.html', category='Default Category')
 
 # Add these lines for the forum routes
 @app.route('/forum/<category>')
@@ -66,6 +33,42 @@ def post_issue(category):
     # Add logic for posting an issue in the given category
     # For now, let's return a simple message
     return f"Post an issue in {category}"
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'username' : request.form['username']})
+
+    if login_user:
+        if bcrypt.checkpw(request.form['password'].encode('utf-8'), login_user['password']):
+            session['username'] = request.form['username']
+            session['category'] = 'Default Category'  # Set a default category
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'username' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert_one({'username' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            session['category'] = 'Default Category'  # Set a default category
+            return redirect(url_for('index'))
+
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('category', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
