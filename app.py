e9b9b3254f729f6cd85de89ea418b5d469c3aa28
Kibrik <ariel@example.com>
2024-03-19 13:48:20 +0200

    Mongodb path reconfigured

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app = Flask(__name__, static_folder='templates/static', static_url_path='/static
 app.secret_key = "admin"
 
 # Configure MongoDB connection
-app.config['MONGO_URI'] = 'mongodb://mongodb.default.svc.cluster.local:27017/'
+app.config['MONGO_URI'] = 'mongodb://mongodb:27017/bugbuddies-db'
 
 mongo = PyMongo(app)
 
-def create_app(mongo_uri='mongodb://mongodb:27017/test'):
+def create_app(mongo_uri='mongodb://mongodb:27017/bugbuddies-db'):
     # Update the existing app configuration
     app.config['MONGO_URI'] = mongo_uri
     
